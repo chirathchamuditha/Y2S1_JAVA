3c88fb7b612a2758223fd9f49dba96eac1a16bdf
chirathchamuditha <chevavitharana@gmail.com>
2024-06-09 23:03:01 +0530

    create my 1st java file

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args){
+        int length =4;
+        int width =5;
+        int area =0;
+
+        area = length * width;
+
+        System.out.println("Length is:" + length);
+        System.out.println("Width is:" + width);
+        System.out.println("Area is:" + area );
+    }
+}
